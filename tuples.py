220e9f3b1e16e01f336a4b5e70b588639b419595
llouliLafuente <141773015+llouliLafuente@users.noreply.github.com>
2024-06-07 12:22:52 -0300

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -8,8 +8,10 @@ def get_coordinate(record):
     :return: str - the extracted map coordinate.
     """
 
-    return ""
-
+    return record[1]
+record = ("Scrimshawed Whale Tooth", "2A")
+coordinate = get_coordinate(record)
+print(coordinate) 
 
 def convert_coordinate(coordinate):
     """Split the given coordinate into tuple containing its individual components.
@@ -18,8 +20,10 @@ def convert_coordinate(coordinate):
     :return: tuple - the string coordinate split into its individual components.
     """
 
-    return ()
-
+    return (coordinate[0], coordinate[1])
+coordinate = "2A"
+converted_coordinate = convert_coordinate(coordinate)
+print(converted_coordinate)
 
 def create_record(azara_record, rui_record):
     """Combine the two record types (if possible) and create a combined record group.
@@ -29,4 +33,15 @@ def create_record(azara_record, rui_record):
     :return: tuple or str - the combined record (if compatible), or the string "not a match" (if incompatible).
     """
 
-    return ()
+    azara_treasure, azara_coordinate = azara_record
+    rui_location, rui_coordinate, rui_quadrant = rui_record
+
+    if azara_coordinate == str(rui_coordinate[0]) + str(rui_coordinate[1]):
+        return (azara_treasure, azara_coordinate, rui_location, rui_coordinate, rui_quadrant)
+    else:
+        return "not a match"
+    
+azara_record = ("Brass Spyglass", "4B")
+rui_record = ("Abandoned Lighthouse", ("4", "B"), "Blue")
+combined_record = create_record(azara_record, rui_record)
+print(combined_record)  
